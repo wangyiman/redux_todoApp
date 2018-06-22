cd57bde056dc2cdd32bc107b524df01cf989dd42
wangyiman <wangyiman@juran.com.cn>
2018-06-22 17:04:39 +0800

    change button to link.

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -1,17 +1,34 @@
-import { setVisibilityFilter } from '../actions'
-import Link from '../components/Link'
-import { connect } from 'react-redux';
+// import { setVisibilityFilter } from '../actions'
+// import Link from '../components/Link'
+// import { connect } from 'react-redux';
 
-const mapStateToProps = (state, ownProps) => ({
-  active: ownProps.filter === state.VisibilityFilters
-})
+// const mapStateToProps = (state, ownProps) => ({
+//   active: ownProps.filter === state.VisibilityFilters
+// })
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  onClick: () => dispatch(setVisibilityFilter(ownProps.filter))
-})
+// const mapDispatchToProps = (dispatch, ownProps) => ({
+//   onClick: () => dispatch(setVisibilityFilter(ownProps.filter))
+// })
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Link);
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(Link);
+
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const FilterLink = ({filter, children}) => (
+  <NavLink
+    to={filter === 'SHOW_ALL' ? '/': `/${ filter }`}
+    activeStyle={{
+      textDecoration: 'none',
+      color: 'black'
+    }}
+  >
+    {children}
+  </NavLink>
+);
+
+export default FilterLink;
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import First from './components/First';
 import rootReducer from './reducers';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <First/>
+    <Router>
+      <Route path=":filter?/" component={First}/>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
